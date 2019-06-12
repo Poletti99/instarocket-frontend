494d4b2ca147b58f49808302ce974028327bc30e
Victor Poletti <victor.poletti@gastecnologia.com.br>
2019-06-12 16:34:28 -0300

    Adicionando Header e rotas da aplicaçao

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import Routes from './routes';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
 function App() {
   return (
-    <div className="App">
-      <h1>Olar bb</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes />
+    </BrowserRouter>
   );
 }
 
